98d5fef9a8c997f52b520e76ece4171ea8616b02
Anton Kurako <goodforgod.dev@gmail.com>
2021-10-02 11:20:18 +0300

    [0.9.0-SNAPSHOT] ResourceHintProcessor improved processing and cleanup

--- src/main/java/io/graalvm/hint/processor/ResourceHintProcessor.java
+++ src/main/java/io/graalvm/hint/processor/ResourceHintProcessor.java
@@ -2,9 +2,7 @@ package io.graalvm.hint.processor;
 
 import io.graalvm.hint.annotation.ResourceHint;
 import java.io.Writer;
-import java.util.Arrays;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
@@ -39,43 +37,50 @@ public class ResourceHintProcessor extends AbstractHintProcessor {
             return false;
         }
 
-//        Map<String, String> options = processingEnv.getOptions();
-//        System.out.println("options: " + options);
-
         final Set<? extends Element> annotated = roundEnv.getElementsAnnotatedWith(ResourceHint.class);
         final Set<TypeElement> types = ElementFilter.typesIn(annotated);
 
-        final String resourceConfigJson = getResourceConfigJsonValue(types);
+        final Optional<String> resourceConfigJson = getResourceConfigJsonValue(types);
+        if (resourceConfigJson.isEmpty()) {
+            processingEnv.getMessager().printMessage(Diagnostic.Kind.MANDATORY_WARNING,
+                    "@ResourceHint found, but patterns are not present");
+            return false;
+        }
 
         final HintOptions hintOptions = getHintOptions(roundEnv);
         final String path = hintOptions.getRelativePathForFile(FILE_NAME);
-
         try {
             final FileObject fileObject = processingEnv.getFiler().createResource(StandardLocation.CLASS_OUTPUT, "", path);
             try (Writer writer = fileObject.openWriter()) {
-                writer.write(resourceConfigJson);
+                writer.write(resourceConfigJson.get());
             }
         } catch (Exception e) {
             processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "Couldn't write " + FILE_NAME + " due to: " + e.getMessage());
             return false;
         }
 
-        processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, "Successfully written " + FILE_NAME + " file to: " + path);
+        processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, "Generated " + FILE_NAME + " file to: " + path);
         return true;
+    }
+
+    private static Optional<String> getResourceConfigJsonValue(Set<TypeElement> types) {
+        final List<String> resources = types.stream()
+                .flatMap(t -> Arrays.stream(t.getAnnotation(ResourceHint.class).patterns()))
+                .filter(r -> !r.isBlank() && !r.equals("."))
+                .collect(Collectors.toList());
+
+        if (resources.isEmpty())
+            return Optional.empty();
+
+        return Optional.of(resources.stream()
+                .map(ResourceHintProcessor::mapToResource)
+                .flatMap(r -> r.entrySet().stream().map(e -> "    { \"" + e.getKey() + "\" : \"" + e.getValue() + "\" }"))
+                .collect(Collectors.joining(",\n", "{\n  \"resources\": [\n", "\n  ]\n}")));
     }
 
     private static Map<String, String> mapToResource(String resourcePattern) {
         return (resourcePattern.contains("*"))
                 ? Map.of(PATTERN, resourcePattern)
                 : Map.of(PATTERN, "\\\\Q" + resourcePattern + "\\\\E");
-    }
-
-    private static String getResourceConfigJsonValue(Set<TypeElement> types) {
-        return types.stream()
-                .flatMap(t -> Arrays.stream(t.getAnnotation(ResourceHint.class).patterns()))
-                .filter(r -> !r.isBlank() && !r.equals("."))
-                .map(ResourceHintProcessor::mapToResource)
-                .flatMap(r -> r.entrySet().stream().map(e -> "    { \"" + e.getKey() + "\" : \"" + e.getValue() + "\" }"))
-                .collect(Collectors.joining(",\n", "{\n  \"resources\": [\n", "\n  ]\n}"));
     }
 }
