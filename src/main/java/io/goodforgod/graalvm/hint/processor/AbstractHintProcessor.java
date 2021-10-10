49d0d4762e6df4be4648f80ff69c28fdbb99055c
Anton Kurako <goodforgod.dev@gmail.com>
2021-10-10 14:08:48 +0300

    [0.9.0-SNAPSHOT] AnnotationTypeFieldVisitor added AbstractHintProcessor#getAnnotationFieldClassNames with parent repeatable annotation support added

--- src/main/java/io/goodforgod/graalvm/hint/processor/AbstractHintProcessor.java
+++ src/main/java/io/goodforgod/graalvm/hint/processor/AbstractHintProcessor.java
@@ -2,16 +2,16 @@ package io.goodforgod.graalvm.hint.processor;
 
 import java.io.Writer;
 import java.lang.annotation.Annotation;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.element.AnnotationValue;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.PackageElement;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.util.ElementFilter;
 import javax.tools.Diagnostic;
 import javax.tools.FileObject;
 import javax.tools.StandardLocation;
@@ -40,6 +40,18 @@ abstract class AbstractHintProcessor extends AbstractProcessor {
         return new HintOptions(group, artifact);
     }
 
+    Set<TypeElement> getAnnotatedElements(RoundEnvironment roundEnv, Class<? extends Annotation>... annotations) {
+        return Arrays.stream(annotations)
+                .flatMap(a -> {
+                    final Set<? extends Element> annotated = roundEnv.getElementsAnnotatedWith(a);
+                    if (annotated == null || annotated.isEmpty())
+                        return Stream.empty();
+
+                    return ElementFilter.typesIn(annotated).stream();
+                })
+                .collect(Collectors.toSet());
+    }
+
     Optional<String> getAnnotationFieldClassNameAny(TypeElement type,
                                                     Class<? extends Annotation> annotation,
                                                     String annotationFieldName) {
@@ -53,15 +65,33 @@ abstract class AbstractHintProcessor extends AbstractProcessor {
                                               Class<? extends Annotation> annotation,
                                               String annotationFieldName) {
         final String annotationName = annotation.getSimpleName();
+        final AnnotationTypeFieldVisitor visitor = new AnnotationTypeFieldVisitor(annotationName, annotationFieldName);
         return type.getAnnotationMirrors().stream()
                 .filter(a -> a.getAnnotationType().asElement().getSimpleName().contentEquals(annotationName))
+                .flatMap(a -> visitor.visitAnnotation(a, "").stream())
+                .collect(Collectors.toList());
+    }
+
+    @SuppressWarnings("unchecked")
+    List<String> getAnnotationFieldClassNames(TypeElement type,
+                                              Class<? extends Annotation> annotation,
+                                              String annotationFieldName,
+                                              Class<? extends Annotation> parentAnnotation) {
+        final String annotationName = annotation.getSimpleName();
+        final String annotationParent = parentAnnotation.getSimpleName();
+        final AnnotationTypeFieldVisitor visitor = new AnnotationTypeFieldVisitor(annotationName, annotationFieldName);
+        return type.getAnnotationMirrors().stream()
+                .filter(a -> a.getAnnotationType().asElement().getSimpleName().contentEquals(annotationParent))
                 .flatMap(a -> a.getElementValues().entrySet().stream()
-                        .filter(e -> e.getKey().getSimpleName().contentEquals(annotationFieldName))
+                        .filter(e -> e.getKey().getSimpleName().contentEquals("value"))
                         .flatMap(e -> {
-                            final Object value = e.getValue().getValue();
-                            return (value instanceof List)
-                                    ? ((List<?>) value).stream().map(Object::toString)
-                                    : Stream.of(value.toString());
+                            final List<String> collect = (List<String>) ((List) e.getValue().getValue()).stream()
+                                    .flatMap(v -> ((AnnotationValue) v).accept(visitor, "").stream())
+                                    .collect(Collectors.toList());
+
+                            processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE,
+                                    "collected - " + collect);
+                            return collect.stream();
                         })
                         .filter(e -> !e.isBlank()))
                 .collect(Collectors.toList());
