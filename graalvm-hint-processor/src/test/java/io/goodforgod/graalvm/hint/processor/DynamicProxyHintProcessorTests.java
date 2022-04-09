20961b4ea645c63b4fa725d3097cb9254e712ecd
Anton Kurako <goodforgod.dev@gmail.com>
2022-04-10 02:35:33 +0300

    [0.18.0-SNAPSHOT] DynamicProxyHintProcessorTests reinforced and improved

--- graalvm-hint-processor/src/test/java/io/goodforgod/graalvm/hint/processor/DynamicProxyHintProcessorTests.java
+++ graalvm-hint-processor/src/test/java/io/goodforgod/graalvm/hint/processor/DynamicProxyHintProcessorTests.java
@@ -15,6 +15,35 @@ import org.junit.jupiter.api.Test;
 class DynamicProxyHintProcessorTests extends ProcessorRunner {
 
     @Test
+    void selfHint() {
+        final Compilation compilation = Compiler.javac()
+                .withProcessors(new NativeImageHintProcessor())
+                .compile(JavaFileObjects.forResource("dynamicproxyhint/source/Self.java"));
+
+        CompilationSubject.assertThat(compilation).succeeded();
+        CompilationSubject.assertThat(compilation)
+                .generatedFile(StandardLocation.CLASS_OUTPUT,
+                        "META-INF/native-image/io.goodforgod.graalvm.hint.processor/hint/native-image.properties")
+                .contentsAsString(StandardCharsets.UTF_8)
+                .isEqualTo(getResourceContentAsString("dynamicproxyhint/generated/native-image-config.properties"));
+
+        CompilationSubject.assertThat(compilation)
+                .generatedFile(StandardLocation.CLASS_OUTPUT,
+                        "META-INF/native-image/io.goodforgod.graalvm.hint.processor/hint/dynamic-proxy-config.json")
+                .contentsAsString(StandardCharsets.UTF_8)
+                .isEqualTo(getResourceContentAsString("dynamicproxyhint/generated/dynamic-proxy-hint-self.json"));
+    }
+
+    @Test
+    void selfClassHintFail() {
+        final Compilation compilation = Compiler.javac()
+                .withProcessors(new NativeImageHintProcessor())
+                .compile(JavaFileObjects.forResource("dynamicproxyhint/source/SelfClass.java"));
+
+        CompilationSubject.assertThat(compilation).failed();
+    }
+
+    @Test
     void configHint() {
         final Compilation compilation = Compiler.javac()
                 .withProcessors(new NativeImageHintProcessor())
